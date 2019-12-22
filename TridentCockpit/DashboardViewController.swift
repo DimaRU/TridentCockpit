de60964b17195939b0c51d3273c36ffa0f2d3a0f
Dmitriy Borovikov <dmitry@borovikov.name>
2019-12-22 15:03:56 +0300

    connect/disconnect WiFI

--- TridentCockpit/DashboardViewController.swift
+++ TridentCockpit/DashboardViewController.swift
@@ -13,9 +13,25 @@ class DashboardViewController: NSViewController {
     let stdParticipantList: Set<String> = ["geoserve", "trident-core", "trident-control", "trident-update", "trident-record"]
     var tridentParticipants: Set<String> = []
     var tridentID: String!
-    var ssid: String?
     weak var toolbar: NSToolbar?
     var popover: NSPopover?
+    var connectedSSID: String? {
+        didSet {
+            guard let wifiItem = toolbar?.getItem(for: .connectWiFi),
+                let button = wifiItem.view as? NSButton else { return }
+            if connectedSSID != nil {
+                button.image = NSImage(named: "wifi.slash")!
+                wifiItem.label = NSLocalizedString("Disconnect WiFi", comment: "")
+                wifiItem.paletteLabel = NSLocalizedString("Disconnect WiFi", comment: "")
+                wifiItem.toolTip = NSLocalizedString("Disconnect Trident WiFi", comment: "")
+            } else {
+                button.image = NSImage(named: "wifi")!
+                wifiItem.label = NSLocalizedString("Connect WiFi", comment: "")
+                wifiItem.paletteLabel = NSLocalizedString("Connect WiFi", comment: "")
+                wifiItem.toolTip = NSLocalizedString("Connect Trident WiFi", comment: "")
+            }
+        }
+    }
 
     
     @IBOutlet weak var gridView: NSGridView!
@@ -71,22 +87,44 @@ class DashboardViewController: NSViewController {
     }
 
     @IBAction func connectWifiButtonPress(_ sender: NSButton) {
-        RestProvider.request(MultiTarget(WiFiServiceAPI.scan))
-        .then {
-                RestProvider.request(MultiTarget(WiFiServiceAPI.ssids))
-        }.done { (ssids: [SSIDInfo]) -> Void in
-            self.showPopup(with: ssids, view: sender.superview!)
+        if connectedSSID == nil {
+            connectWiFi(view: sender.superview!)
+        } else {
+            disconnectWiFi()
+        }
+    }
+
+    private func disconnectWiFi() {
+        RestProvider.request(MultiTarget(WiFiServiceAPI.disconnect))
+        .then { _ in
+            RestProvider.request(MultiTarget(WiFiServiceAPI.clear))
+        }.done {
+            self.connectedSSID = nil
         }.catch { error in
             print(error)
         }
     }
-    
+
+    private func connectWiFi(view: NSView) {
+        RestProvider.request(MultiTarget(WiFiServiceAPI.scan))
+        .then {
+            after(seconds: 0.25)
+        }.then {
+            RestProvider.request(MultiTarget(WiFiServiceAPI.ssids))
+        }.done { (ssids: [SSIDInfo]) -> Void in
+            self.showPopup(with: ssids, view: view)
+        }.catch { error in
+            print(error)
+        }
+    }
+
     private func showPopup(with ssids: [SSIDInfo], view: NSView) {
         guard let controller = NSStoryboard.main?.instantiateController(withIdentifier: "WiFiPopupViewController") as? WiFiPopupViewController else { return }
         popover = NSPopover()
         popover?.contentViewController = controller
         popover?.animates = true
         popover?.behavior = .transient
+        popover?.delegate = self
         controller.delegate = self
         controller.ssids = ssids
         popover?.show(relativeTo: .zero, of: view, preferredEdge: .minY)
@@ -129,16 +167,16 @@ class DashboardViewController: NSViewController {
         localAddressLabel.stringValue = FastRTPS.localAddress
         
         RestProvider.request(MultiTarget(ResinAPI.deviceState))
-            .then { (deviceState: DeviceState) -> Promise<[ConnectionInfo]> in
-                print(deviceState)
-                self.tridentNetworkAddressLabel.stringValue = deviceState.ipAddress
-                return RestProvider.request(MultiTarget(WiFiServiceAPI.connection))
+        .then { (deviceState: DeviceState) -> Promise<[ConnectionInfo]> in
+            print(deviceState)
+            self.tridentNetworkAddressLabel.stringValue = deviceState.ipAddress
+            return RestProvider.request(MultiTarget(WiFiServiceAPI.connection))
         }.done { connectionInfo in
             print(connectionInfo)
             if let ssid = connectionInfo.first(where: {$0.kind == "802-11-wireless"})?.ssid,
                 let textField = self.view.window?.toolbar?.getItem(for: .wifiSSID)?.view as? NSTextField {
                 textField.stringValue = ssid
-                self.ssid = ssid
+                self.connectedSSID = ssid
             }
         }.catch { error in
             print(error)
@@ -147,22 +185,7 @@ class DashboardViewController: NSViewController {
             toolbar.getItem(for: .goDive)?.isEnabled = true
             toolbar.getItem(for: .goMaintenance)?.isEnabled = true
             toolbar.getItem(for: .goPastDives)?.isEnabled = true
-            if let wifiItem = toolbar.getItem(for: .connectWiFi) {
-                wifiItem.isEnabled = true
-                if ssid != nil {
-                    wifiItem.image = NSImage(named: "wifi.slash")!
-                    wifiItem.label = NSLocalizedString("Disconnect WiFi", comment: "")
-                    wifiItem.paletteLabel = NSLocalizedString("Disconnect WiFi", comment: "")
-                    wifiItem.toolTip = NSLocalizedString("Disconnect Trident WiFi", comment: "")
-                } else {
-                    wifiItem.image = NSImage(named: "wifi")!
-                    wifiItem.label = NSLocalizedString("Connect WiFi", comment: "")
-                    wifiItem.paletteLabel = NSLocalizedString("Connect WiFi", comment: "")
-                    wifiItem.toolTip = NSLocalizedString("Connect Trident WiFi", comment: "")
-                }
-            }
         }
-        
         FastRTPS.setPartition(name: tridentID)
     }
     
@@ -200,5 +223,10 @@ extension DashboardViewController: WiFiPopupDelegate {
         self.popover?.performClose(nil)
         self.popover = nil
     }
-    
+}
+
+extension DashboardViewController: NSPopoverDelegate {
+    func popoverDidClose(_ notification: Notification) {
+        self.popover = nil
+    }
 }
