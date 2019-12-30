e7d41c68b799357461c8d63eb4fd72aafb0cd2be
Dmitriy Borovikov <dmitry@borovikov.name>
2019-12-31 00:41:41 +0300

    Bug fix

---
+++ TridentCockpit/DDSModel/RovIMUCalibration.swift
@@ -0,0 +1,26 @@
+/////
+////  RovIMUCalibration.swift
+///   Copyright © 2019 Dmitriy Borovikov. All rights reserved.
+//
+
+import Foundation
+import FastRTPSBridge
+
+struct RovIMUCalibration: DDSKeyed {
+    enum Calibration: UInt8, Codable {
+        case uncalibrated    = 0
+        case poor            = 1
+        case fair            = 2
+        case excellent       = 3
+    }
+    // Pertains mainly to the BNO055 with its concept of 0-3 calibration readiness values
+    let id: String  //@key
+
+    let accel: Calibration    // Calibration state of the accelerometer
+    let gyro: Calibration     // Calibration state of the gyro
+    let mag: Calibration      // Calibration state of the mag
+    let system: Calibration   // Calibration state of the system
+    
+    var key: Data { id.data(using: .utf8)! }
+    static var ddsTypeName: String { "orov::msg::system::IMUCalibration" }
+}
--- TridentCockpit/MaintenanceViewController.swift
+++ TridentCockpit/MaintenanceViewController.swift
@@ -8,22 +8,23 @@ import Cocoa
 
 class MaintenanceViewController: NSViewController {
     @IBOutlet weak var batteryChargeLabel: NSTextField!
+    @IBOutlet weak var batteryCycleLabel: NSTextField!
     @IBOutlet weak var internalPressureLabel: NSTextField!
     @IBOutlet weak var internalTemperatureLabel: NSTextField!
     @IBOutlet weak var pressureLabel: NSTextField!
     @IBOutlet weak var temperatureLabel: NSTextField!
-
+    
     private var pressure: Double = 0 {
-        didSet { pressureLabel.stringValue = String(format: "%.3fkPa", pressure) }
+        didSet { pressureLabel.stringValue = String(format: "%.3f kPa", pressure) }
     }
     private var internalPressure: Double = 0 {
-        didSet { internalTemperatureLabel.stringValue = String(format: "%.3fkPa", internalPressure) }
+        didSet { internalPressureLabel.stringValue = String(format: "%.3f kPa", internalPressure) }
     }
     private var temperature: Double = 0 {
-        didSet { temperatureLabel.stringValue = String(format: "%.1f℃", temperature) }
+        didSet { temperatureLabel.stringValue = String(format: "%.2f", temperature) }
     }
     private var internalTemperature: Double = 0 {
-        didSet { internalTemperatureLabel.stringValue = String(format: "%.1f℃", internalTemperature) }
+        didSet { internalTemperatureLabel.stringValue = String(format: "%.2f", internalTemperature) }
     }
 
     override func viewDidLoad() {
@@ -46,27 +47,70 @@ class MaintenanceViewController: NSViewController {
 
         FastRTPS.registerReader(topic: .rovDepth) { [weak self] (depth: RovDepth) in
             DispatchQueue.main.async {
-                self?.pressure = depth.pressure.fluidPressure
+                self?.pressure = depth.pressure.fluidPressure / 10
             }
         }
 
         FastRTPS.registerReader(topic: .rovTempInternal) { [weak self] (temp: RovTemperature) in
             DispatchQueue.main.async {
-                self?.temperature = temp.temperature.temperature
+                self?.internalTemperature = temp.temperature.temperature
             }
         }
 
         FastRTPS.registerReader(topic: .rovPressureInternal) { [weak self] (baro: RovBarometer) in
             DispatchQueue.main.async {
-                self?.pressure = baro.pressure.fluidPressure
+                self?.internalPressure = baro.pressure.fluidPressure / 10
+            }
+        }
+
+        FastRTPS.registerReader(topic: .rovFuelgaugeStatus) { [weak self] (status: RovFuelgaugeStatus) in
+            DispatchQueue.main.async {
+                self?.batteryChargeLabel.stringValue = String(format: "%.0f%%", status.state.percentage * 100)
             }
         }
 
         FastRTPS.registerReader(topic: .rovFuelgaugeHealth) { [weak self] (health: RovFuelgaugeHealth) in
             DispatchQueue.main.async {
-                self?.batteryChargeLabel.stringValue = "\(health.state.percentage)%"
+                self?.batteryCycleLabel.stringValue = String(health.cycle_count)
             }
         }
+
+//        FastRTPS.registerReader(topic: .rovSubsystemStatus) { (status: RovSubsystemStatus) in
+//            print("status:", status.subsystemId.rawValue, status.substate)
+//        }
+//        FastRTPS.registerReader(topic: .rovFirmwareStatus) { (firmwareStatus: RovFirmwareStatus) in
+//            print(firmwareStatus)
+//        }
+//        FastRTPS.registerReader(topic: .rovFirmwareServiceStatus) { (firmwareServiceStatus: RovFirmwareServiceStatus) in
+//            print(firmwareServiceStatus)
+//        }
+//        FastRTPS.registerReader(topic: .rovFirmwareCommandRep) { (command: RovFirmwareCommand) in
+//            print(command)
+//        }
+//        FastRTPS.registerReader(topic: .rovControlCurrent) { (control: RovTridentControlTarget) in
+//            print(control)
+//        }
+//        FastRTPS.registerReader(topic: .navTrackingCurrent) { (cameraObjectTrack: RovCameraObjectTrack) in
+//            print(cameraObjectTrack)
+//        }
+//        FastRTPS.registerReader(topic: .mcuI2cStats) { (stats: I2CStats) in
+//            print(stats)
+//        }
+//        FastRTPS.registerReader(topic: .rovSafety) { (state: RovSafetyState) in
+//            print(state)
+//        }
+//        FastRTPS.registerReader(topic: .rovAttitude) { (attitude: RovAttitude) in
+//            print(attitude)
+//        }
+//        FastRTPS.registerReader(topic: .rovImuCalibration) { (calibration: IMUCalibration) in
+//            print(calibration)
+//        }
+//        FastRTPS.registerReader(topic: .rovDepthConfigCurrent) { (config: RovDepthConfig) in
+//            print(config)
+//        }
+//        FastRTPS.registerReader(topic: .rovControllerStateCurrent) { (status: RovControllerStatus) in
+//            print(status)
+//        }
 
     }
 }
