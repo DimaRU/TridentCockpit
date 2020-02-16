c607f0abca216c4156ac90f07e3fa32b1b314111
Dmitriy Borovikov <dmitry@borovikov.name>
2020-02-16 13:03:59 +0300

    Trident Cockpit iOS target

---
+++ TridentCockpitiOS/ViewController.swift
@@ -0,0 +1,18 @@
+/////
+////  ViewController.swift
+///   Copyright © 2020 Dmitriy Borovikov. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
