adbf48b9ae884e603857f68729c7bf758b0b942b
Dmitriy Borovikov <dmitry@borovikov.name>
2020-02-20 23:46:25 +0300

    DiveViewController beta 3

--- TridentCockpitiOS/FloatingView.swift
+++ TridentCockpitiOS/FloatingView.swift
@@ -7,19 +7,28 @@ import UIKit
 
 class FloatingView: UIView {
 
+    private var xConstraint: NSLayoutConstraint?
+    private var yConstraint: NSLayoutConstraint?
+
     var alignConst: CGFloat { 10 }
     private var viewCenter: CGPoint!
     private var isAlignFeedbackSent = false
 
-    override func didMoveToSuperview() {
-        super.didMoveToSuperview()
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
 
+        guard window != nil, let superview = superview else { return }
         let recognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureHandler(recognizer:)))
         self.addGestureRecognizer(recognizer)
         borderColor = .gray
 
         let cp = loadPosition() ?? loadDefaults()
-        center = CGPoint(x: cp.x * superview!.frame.width, y: cp.y * superview!.frame.height)
+        let x = cp.x * superview.frame.width
+        let y = cp.y * superview.frame.height
+
+        xConstraint = centerXAnchor.constraint(equalTo: superview.leadingAnchor, constant: x)
+        yConstraint = centerYAnchor.constraint(equalTo: superview.topAnchor, constant: y)
+        NSLayoutConstraint.activate([xConstraint!, yConstraint!])
     }
 
     @objc func panGestureHandler(recognizer: UIPanGestureRecognizer) {
@@ -36,7 +45,7 @@ class FloatingView: UIView {
             let superFrame = superview!.frame
             let translation = recognizer.translation(in: view.superview!)
             var newOrigin = CGPoint(x: viewCenter!.x + translation.x, y: viewCenter!.y + translation.y)
-            
+
             // stick to center
             let yPosWhenCenter = superFrame.height / 2
             if abs(newOrigin.y - yPosWhenCenter) <= alignConst {
@@ -54,7 +63,9 @@ class FloatingView: UIView {
             let xMax = superFrame.width - frame.width
             let yMax = superFrame.height - frame.height
             newOrigin = newOrigin.constrained(to: CGRect(x: frame.width / 2, y: frame.height / 2, width: xMax, height: yMax))
-            view.center = newOrigin
+
+            xConstraint?.constant = newOrigin.x
+            yConstraint?.constant = newOrigin.y
         case .ended, .cancelled:
             viewCenter = view.center
             view.borderWidth = 0
@@ -65,25 +76,23 @@ class FloatingView: UIView {
             break
         }
     }
-    
-    func superViewDidResize(to size: CGSize) {
-        assert(superview != nil)
 
-        let cph = center.x / superview!.frame.width
-        let cpv = center.y / superview!.frame.height
-
-        center = CGPoint(x: cph * size.width, y: cpv * size.height)
-        savePosition(cp: CGPoint(x: cph, y: cpv))
-    }
-
-   
     func loadDefaults() -> CGPoint {
         CGPoint(x: 0, y: 0)
     }
 
     func savePosition(cp: CGPoint) {}
-    
+
     func loadPosition() -> CGPoint? {
         nil
+    }
+
+    func superViewDidResize(to size: CGSize) {
+        assert(superview != nil)
+        let cph = center.x / superview!.frame.width
+        let cpv = center.y / superview!.frame.height
+
+        xConstraint?.constant = cph * size.width
+        yConstraint?.constant = cpv * size.height
     }
 }
