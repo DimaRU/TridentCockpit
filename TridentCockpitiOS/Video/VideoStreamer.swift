327450d9f7f93994630c9173bc4153ce0366f502
Dmitriy Borovikov <dmitry@borovikov.name>
2020-06-26 19:03:02 +0300

    VideoStreamer error support

--- TridentCockpitiOS/Video/VideoStreamer.swift
+++ TridentCockpitiOS/Video/VideoStreamer.swift
@@ -8,21 +8,48 @@ import AVFoundation
 import VideoToolbox
 
 protocol VideoStreamerDelegate: class {
-    func state(connected: Bool)
+    func state(published: Bool)
     func stats(fps: UInt16, bytesOutPerSecond: Int32, totalBytesOut: Int64)
+    func showError(_ error: StreamerError)
+}
+
+enum StreamerError: Error {
+    case publishingError
+    case connectionError
+}
+extension StreamerError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .publishingError: return "Publishing error"
+        case .connectionError: return "Connection error"
+        }
+    }
 }
 
 class VideoStreamer: NSObject, VideoProcessorDelegate {
+    enum State {
+        case disconnected, connected, publishing, published
+    }
+    weak var delegate: VideoStreamerDelegate?
+    var isPublished: Bool { state == .published }
+    
     private var streamKey: String
     private var streamURL: String
     private var retryCount: Int = 0
     private var observation: NSKeyValueObservation?
-    weak var delegate: VideoStreamerDelegate?
-
+    
+    private var state: State {
+        didSet {
+            guard state != oldValue else { return }
+            delegate?.state(published: isPublished)
+        }
+    }
+    
     private lazy var rtmpConnection: RTMPConnection = {
         let connection = RTMPConnection()
         connection.addEventListener(.rtmpStatus, selector: #selector(rtmpStatusEvent), observer: self)
         connection.addEventListener(.ioError, selector: #selector(rtmpErrorHandler), observer: self)
+        //        connection.requireNetworkFramework = true
         return connection
     }()
     
@@ -34,9 +61,12 @@ class VideoStreamer: NSObject, VideoProcessorDelegate {
     init(url: String, key: String) {
         streamKey = key
         streamURL = url
+        state = .disconnected
+        super.init()
     }
     
     deinit {
+        print(#file, #function)
         observation?.invalidate()
         rtmpConnection.removeEventListener(.ioError, selector: #selector(rtmpErrorHandler), observer: self)
         rtmpConnection.removeEventListener(.rtmpStatus, selector: #selector(rtmpStatusEvent), observer: self)
@@ -45,12 +75,15 @@ class VideoStreamer: NSObject, VideoProcessorDelegate {
     }
     
     func cleanup() {
+        rtmpStream.close()
+        rtmpStream.dispose()
         disconnect()
     }
-
+    
     func connect() {
-        rtmpConnection.connect(streamURL, arguments: nil)
-        observation = observe(\.rtmpStream.currentFPS, options: .new) { (object, change) in
+        rtmpConnection.connect(streamURL)
+        observation = observe(\.rtmpStream.currentFPS, options: .new) { [weak self] (object, change) in
+            guard let self = self else { return }
             self.delegate?.stats(fps: self.rtmpStream.currentFPS,
                                  bytesOutPerSecond: self.rtmpConnection.currentBytesOutPerSecond,
                                  totalBytesOut: self.rtmpConnection.totalBytesOut)
@@ -62,6 +95,7 @@ class VideoStreamer: NSObject, VideoProcessorDelegate {
     }
     
     func disconnect() {
+        state = .disconnected
         rtmpConnection.close()
     }
     
@@ -82,7 +116,7 @@ class VideoStreamer: NSObject, VideoProcessorDelegate {
         ]
         rtmpStream.audioSettings = [.bitrate: 32000]
     }
-        
+    
     func set(format: CMVideoFormatDescription, time: CMTime) {
         rtmpStream.mixer.videoIO.formatDescription = format
         rtmpStream.muxer.didSetFormatDescription(video: format)
@@ -96,43 +130,68 @@ class VideoStreamer: NSObject, VideoProcessorDelegate {
     private func rtmpErrorHandler(_ notification: Notification) {
         let e = Event.from(notification)
         print("Error:", e)
+        delegate?.showError(.connectionError)
+        state = .disconnected
         rtmpConnection.connect(streamURL)
     }
     
     @objc
     private func rtmpStatusEvent(_ notification: Notification) {
         let e = Event.from(notification)
-        print(e)
         guard
             let data: ASObject = e.data as? ASObject,
             let code: String = data["code"] as? String else {
+                print("---- No code event:", e)
                 return
         }
         print("rtmp status:", code)
         switch code {
+        case RTMPConnection.Code.connectFailed.rawValue:
+            state = .disconnected
+            rtmpConnection.close()
+            delegate?.showError(.connectionError)
         case RTMPConnection.Code.connectRejected.rawValue:
+            state = .disconnected
+            delegate?.showError(.connectionError)
             break
         case RTMPConnection.Code.connectSuccess.rawValue:
-            print("Connection success")
             retryCount = 0
             rtmpStream.publish(streamKey)
+            state = .publishing
         case RTMPStream.Code.publishFailed.rawValue:
-            break
+            state = .disconnected
+            disconnect()
+            delegate?.showError(.publishingError)
         case RTMPStream.Code.unpublishSuccess.rawValue:
-            break
+            disconnect()
+            state = .disconnected
         case RTMPStream.Code.publishStart.rawValue:
-            delegate?.state(connected: true)
-        case RTMPConnection.Code.connectFailed.rawValue,
-             RTMPConnection.Code.connectClosed.rawValue:
-            delegate?.state(connected: false)
-            guard retryCount < 5 else {
+            state = .published
+        case "NetStream.Unpublish.InternalKill":
+            // Freezed stream, must reconnect/republish
+            state = .connected
+        case RTMPConnection.Code.connectClosed.rawValue:
+            guard state != .publishing else {
+                state = .disconnected
+                // Wrong publishing
+                delegate?.showError(.publishingError)
                 return
             }
-            DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 2) {
+            guard state != .disconnected else {
+                return
+            }
+            fallthrough
+        case RTMPConnection.Code.connectFailed.rawValue:
+            state = .disconnected
+            guard retryCount < 5 else {
+                delegate?.showError(.connectionError)
+                break
+            }
+            DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 5) {
                 self.rtmpConnection.connect(self.streamURL)
             }
         default:
-            print(e)
+            print("---- Unknown event", e)
             break
         }
     }
