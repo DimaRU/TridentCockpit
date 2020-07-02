5c3aa2c6df76970e763886c31ad5d72e6c1e714b
Dmitriy Borovikov <dmitry@borovikov.name>
2020-07-02 21:34:22 +0300

    Start RecordingsAPI early

--- TridentCockpitiOS/Etc/AppDelegate.swift
+++ TridentCockpitiOS/Etc/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Instantiate Recordings API
+        _ = RecordingsAPI.shared
 
         return true
     }
